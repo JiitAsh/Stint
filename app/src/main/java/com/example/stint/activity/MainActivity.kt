ddbc5c16c4c2f58c41e319759d45229a2de62037
JiitAsh <jitmarwal@gmail.com>
2024-06-21 00:28:40 +0530

    Saving user input ot DB

--- app/src/main/java/com/example/stint/activity/MainActivity.kt
+++ app/src/main/java/com/example/stint/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.stint.activity
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,11 +19,7 @@ import com.example.stint.model.Task
 class MainActivity : AppCompatActivity() {
 
     var dbHandler : TasksDatabaseHandler? = null
-    var enterTask = findViewById<EditText>(R.id.enterTaskId)
-    var assignTo = findViewById<EditText>(R.id.assignToId)
-    var assignedBy = findViewById<EditText>(R.id.assignedById)
-    var saveATask = findViewById<Button>(R.id.saveTaskBtn)
-
+//    var progressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,16 +31,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+//        progressDialog = ProgressDialog(this)
+
         dbHandler = TasksDatabaseHandler(this)
 
+        var enterTask = findViewById<EditText>(R.id.enterTaskId)
+        var assignTo = findViewById<EditText>(R.id.assignToId)
+        var assignedBy = findViewById<EditText>(R.id.assignedById)
+        var saveATask = findViewById<Button>(R.id.saveTaskBtn)
+
         saveATask.setOnClickListener {
+            /* ToDo: remove progress dialog, it's not working properly */
+//            progressDialog!!.setMessage("Saving Task...")
+//            progressDialog!!.show()
 
             if(!TextUtils.isEmpty(enterTask.text.toString())
                 && !TextUtils.isEmpty(assignTo.text.toString())
                 && !TextUtils.isEmpty(assignedBy.text.toString())){
 
                 // save task to the database
+                var task = Task()
+                task.taskName = enterTask.text.toString()
+                task.assignedBy = assignedBy.text.toString()
+                task.assignedTo = assignTo.text.toString()
+//                task.taskName = findViewById<EditText>(R.id.enterTaskId).text.toString()
+//                task.assignedBy = findViewById<EditText>(R.id.assignedById).text.toString()
+//                task.assignedTo = findViewById<EditText>(R.id.assignToId).text.toString()
 
+                saveToDB(task)
+//                progressDialog!!.cancel()
+                startActivity(Intent(this,TaskListActivity::class.java))
+
+            }else{
+                Toast.makeText(this, "Please enter a task", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -59,5 +81,10 @@ class MainActivity : AppCompatActivity() {
 //        var tasks: Task = dbHandler!!.readATask(1)
 //
 //        Log.d("Item: ",tasks.taskName.toString())
+    }
+
+
+    fun saveToDB(task:Task){
+        dbHandler!!.createTask(task)
     }
 }
